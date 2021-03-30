fa56c84f11004ae1728652d8aa4970a8bc36ae90
gxd <gxd523@gmail.com>
2021-03-30 15:15:07 +0800

    no message

--- library/src/main/java/com/gxd/viewbindingwrapper/ViewBindingActivity.kt
+++ library/src/main/java/com/gxd/viewbindingwrapper/ViewBindingActivity.kt
@@ -1,10 +1,10 @@
 package com.gxd.viewbindingwrapper
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.viewbinding.ViewBinding
 
-abstract class ViewBindingActivity<T : ViewBinding> : Activity() {
+abstract class ViewBindingActivity<T : ViewBinding> : ComponentActivity() {
     private var _binding: T? = null
     val binding: T by lazy {
         val viewBindingClass = ViewBindingUtil.getViewBindingClass<T>(this@ViewBindingActivity)
